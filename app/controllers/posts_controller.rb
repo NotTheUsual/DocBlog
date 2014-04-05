4ce0544620d87753ba4d0b2531487a11802419e2
James Hunter <james.hunter@me.com>
2014-04-05 16:38:45 +0200

    Basic styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,23 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.map do |post|
-      HTML::Pipeline::MarkdownFilter.new(post.body, :gfm => false).call
+      HTML::Pipeline::MarkdownFilter.new(post.body).call
     end
   end
 
   def admin_index
     @posts = Post.all
+    render layout: 'admin'
   end
 
   def new
     @post = "post"
+    render layout: 'admin'
   end
 
   def create
     doc = params[:post][:doc]
     Post.upload(doc)
-    redirect_to new_post_path
+    redirect_to posts_path
   end
 end
